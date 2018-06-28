4c264880f0ccc973d6191c54d76fe492f0e04b95
Rodrigo Ramos Guimaraes <rguimaraes@windowslive.com>
2018-06-28 16:56:34 -0300

    wip parallel branch test

--- bie.evgestao/3 - Domain/3.1 - Domain/bie.evgestao.domain/Interfaces/Repository/ImplementacaoPadrao.cs
+++ bie.evgestao/3 - Domain/3.1 - Domain/bie.evgestao.domain/Interfaces/Repository/ImplementacaoPadrao.cs
@@ -3,5 +3,8 @@
 namespace bie.evgestao.domain.Interfaces.Repository
 {
     public interface IPessoaRepository : IRepositoryBase<Pessoa> { }
+    public interface IFamiliarRepository : IRepositoryBase<Familiar> { }
+    public interface ICelulaRepository : IRepositoryBase<Celula> { }
+    public interface IFotoRepository : IRepositoryBase<Foto> { }
 
 }
